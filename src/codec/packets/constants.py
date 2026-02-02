49fd4e768b94298f8b238006523a5dbf3ac75c9c
Cry2003 <christianvillani211103@gmail.com>
2026-02-02 22:19:20 +0100

    Add identifier.py constants

--- src/codec/packets/constants.py
+++ src/codec/packets/constants.py
@@ -1,7 +1,13 @@
 # src/codec/constants.py
 
+import re
+
 # packet.py constants
-_MAX_VARINT_3_BYTES = 0x1FFFFF          # 2097151 (2^21 - 1)
+_MAX_VARINT_3_BYTES = 0x1FFFFF  # 2097151 (2^21 - 1)
 
 # Maximum packet size representable by a 3-byte VarInt (protocol limit)
 _MAX_PACKET_LENGTH = 2**21 - 1  # 2097151
+
+# identifier.py constants
+_NAMESPACE_RE = re.compile(r"^[a-z0-9._-]+$")
+_VALUE_RE = re.compile(r"^[a-z0-9._/-]+$")
